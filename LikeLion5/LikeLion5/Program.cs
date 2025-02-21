ca96006bee6d65bdbaf9ff4a949c1766dd00fa0e
WnoonW <wonwoo0406@naver.com>
2025-02-21 14:35:12 +0900

    study5

---
+++ LikeLion5/LikeLion5/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int integerNum = 10;
+            float floatNum = 3.14f;
+            double doubleNum = 3.14159;
+
+            Console.WriteLine(integerNum); Console.WriteLine(floatNum); Console.WriteLine(doubleNum);*/
+
+            /*int intValue = -100;
+            long longValue = 1234567890L;
+            Console.WriteLine(intValue);
+            Console.WriteLine(longValue);*/
+
+            /*sbyte signedByte = -50;
+            short signedShort = -32000;
+            int signedInt = -200000000;
+            Console.WriteLine(signedByte);
+            Console.WriteLine(signedInt);
+            Console.WriteLine(signedShort);*/
+
+            /*byte unsignedByte = 255;
+            ushort unsignedShort = 65000;
+            uint unsignedInt = 4000000000;
+
+            Console.WriteLine(unsignedByte);
+            Console.WriteLine(unsignedShort);
+            Console.WriteLine(unsignedInt);*/
+
+            /*float singlePrecision = 3.14f;
+            double doublePrecision = 3.1415926535;
+            decimal highPrecision = 3.141592653588979141234m;
+
+            Console.WriteLine(singlePrecision);
+            Console.WriteLine(doublePrecision);
+            Console.WriteLine(highPrecision);*/
+
+            /*char letter = 'A';
+            char symbol = '#';
+            char number = '9';
+
+            Console.WriteLine(letter);
+            Console.WriteLine(symbol);
+            Console.WriteLine(number);*/
+
+            /*string greeting = "Hello World";
+            string name = "Alice";
+            Console.WriteLine(greeting);
+            Console.WriteLine(name);
+            Console.WriteLine(greeting+ " " +name);*/
+
+            /*bool isRunning = true;
+            bool isFinished = false;
+
+            Console.WriteLine(isRunning);
+            Console.WriteLine(isFinished);*/
+
+            /*const double Pi = 3.14159;
+            const int MAXSCORE = 100;
+
+            Console.WriteLine(Pi);
+            Console.WriteLine(MAXSCORE);*/
+
+            /*System.Int32 number = 123;
+            System.String text = "Hello";
+            System.Boolean flag = true;
+
+            Console.WriteLine(number);
+            Console.WriteLine(text);
+            Console.WriteLine(flag);*/
+
+            int number = 123;
+            string numberToString = number.ToString();
+
+            bool flag = true;
+            string flagToString = flag.ToString();
+
+            Console.WriteLine(numberToString);
+            Console.WriteLine(flagToString);
+
+
+
+
+        }
+    }
+}
