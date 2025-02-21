2134840c7eca2ff6551bdcc3ff86365d4b0fd5bf
WnoonW <wonwoo0406@naver.com>
2025-02-21 10:43:05 +0900

    study3

---
+++ LikeLion3/LikeLion3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hp = 100;
+            double att = 56.7;
+            char rate = 'S';
+            string character = "Character";
+            string CharacterName = "AA";
+
+            Console.WriteLine(character);
+            Console.WriteLine(hp);
+            Console.WriteLine(att);
+            Console.WriteLine(rate);
+            Console.WriteLine(CharacterName);
+        }
+    }
+}
