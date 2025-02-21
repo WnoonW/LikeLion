21172181b87f08aaf58f3cc3a3caa24c509e4e6e
WnoonW <wonwoo0406@naver.com>
2025-02-21 16:03:44 +0900

    study7

---
+++ LikeLion6/LikeLion6/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.Write("이름을 입력하세요: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"안녕하세요, {userName}님!");*/
+
+            /*Console.Write("나이를 입력하세요: ");
+            string input = Console.ReadLine();
+            int age = int.Parse(input);               //Parse 문자형식을 int 형식으로
+
+            Console.WriteLine($"내년에는 {age + 1} 살이 되겠군요!");*/
+
+            /*Console.Write("루인 스킬 피해: ");
+            string input1 = Console.ReadLine();
+            float stat1 = float.Parse(input1);
+            Console.Write("%\b");
+            Console.Write("카드 게이지 획득량: ");
+            string input2 = Console.ReadLine();
+            float stat2 = float.Parse(input2);
+            Console.Write("각성기 피해: ");
+            string input3 = Console.ReadLine();
+            float stat3 = float.Parse(input3);
+            Console.Write("최대 마나: ");
+            string input4 = Console.ReadLine();
+            float stat4 = float.Parse(input4);
+            Console.Write("전투 중 마나 회복량: ");
+            string input5 = Console.ReadLine();
+            float stat5 = float.Parse(input5);
+            Console.Write("비전투 중 마나 회복량: ");
+            string input6 = Console.ReadLine();
+            float stat6 = float.Parse(input6);
+            Console.Write("이동 속도: ");
+            string input7 = Console.ReadLine();
+            float stat7 = float.Parse(input7);
+            Console.Write("탈 것 속도: ");
+            string input8 = Console.ReadLine();
+            float stat8 = float.Parse(input8);
+            Console.Write("운반 속도: ");
+            string input9 = Console.ReadLine();
+            float stat9 = float.Parse(input9);
+            Console.Write("스킬 재사용 대기시간 감소: ");
+            string input10 = Console.ReadLine();
+            float stat10 = float.Parse(input10);*/
+
+            Console.Write("입력하세요: ");
+            char key = Console.ReadKey().KeyChar;
+
+            Console.Write("%");
+
+
+
+        }
+    }
+}
