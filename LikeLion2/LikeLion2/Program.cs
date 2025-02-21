729a00456d86a52a9fe997272a6c04ea54529c89
WnoonW <wonwoo0406@naver.com>
2025-02-21 10:22:45 +0900

    study2

---
+++ LikeLion2/LikeLion2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            age = 24;
+            Console.Write(age);
+        }
+    }
+}
