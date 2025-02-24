6a70b1b0acf877d9101e00cd81d3eb4464572ede
WnoonW <wonwoo0406@naver.com>
2025-02-24 10:33:12 +0900

    study02241

---
+++ 02-24-LikeLion1/02-24-LikeLion1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_24_LikeLion1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*//이진수를 정수로 변환
+            Console.Write("2진수를 입력하세요:");
+            string binaryInput = Console.ReadLine();  //입력받는다 문자열
+            int decimalValue = Convert.ToInt32(binaryInput, 2); //2진수 -> 10진수 변환
+
+            //정수를 이진수로 변환
+            string binaryOutput = Convert.ToString(decimalValue, 2);//10진수 ->2진수
+
+            Console.WriteLine($"입력한 이진수: {binaryInput}");
+            Console.WriteLine($"10진수로변환 : {decimalValue}");
+            Console.WriteLine($"다시 이진수로 변환 :{binaryOutput}");*/
+
+            /*var name = "Alice";
+            var age = 25;
+            var isStudent = true;
+            Console.WriteLine($"이름:{name}\n나이:{age}\n학생 여부:{isStudent}");*/
+
+            /*int defaultInt = default;
+            string defaultString = default;
+            bool defaultBool = default;
+            Console.WriteLine($"{defaultInt},{defaultString},{defaultBool}");*/
+
+            /*int a = 5, b = 3;
+            int sum = default;
+            sum = a + b;
+            Console.WriteLine($"합:{sum}");
+            sum = a - b;
+            Console.WriteLine($"차:{sum}");
+            sum = a * b;
+            Console.WriteLine($"곱:{sum}");
+            sum = a / b;
+            Console.WriteLine($"셈:{sum}");
+            sum = a % b;
+            Console.WriteLine($"나머지:{sum}");*/
+
+            /*int number = 7;
+            int sum = 0;
+            sum = number % 2;
+            Console.WriteLine("짝수 홀수 판별 : " + sum);*/
+
+            /*bool isEqual = false; // 거짓 0
+            int a = 5;
+            int b = 5;
+            //관계형 연산자
+            isEqual = (a == b); //a 랑 b랑 같은가?
+            Console.WriteLine("같은가? " + isEqual);*/
+
+
+        }
+    }
+}
