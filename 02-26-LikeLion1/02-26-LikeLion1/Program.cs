99c7e34ab20f2bd7291f1034baf693a08ce17b9e
WnoonW <wonwoo0406@naver.com>
2025-02-26 16:09:48 +0900

    function

---
+++ 02-26-LikeLion1/02-26-LikeLion1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_26_LikeLion1
+{
+    class Program
+    {
+
+        /*static void AttackFunction(int Attack) { Console.WriteLine("공격력은 : " + Attack); }
+        static int BaseAttack() { int attack = 10; return attack; }*/
+
+
+        /*static int Add(int a, int b) { int sum = 0; sum = a + b; return sum; }*/
+
+
+
+        static void Main(string[] args)
+        {
+            /*int Attack = 0;
+            int BAttack = 0;
+            Console.WriteLine("캐릭터의 공격력을 입력 : ");
+            Attack = int.Parse(Console.ReadLine());
+
+            BAttack = BaseAttack();
+            AttackFunction(Attack+BAttack);*/
+
+            /*int result = Add(10, 20);
+            Console.WriteLine($"10 + 20 = {result}");*/
+
+            /*string[] fruits = { "사과", "바나나", "체리" };
+            foreach (string fruit in fruits) Console.WriteLine(fruit);*/
+
+        }
+    }
+}
