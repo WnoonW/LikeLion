a49f03a94e04221403420c995f5e939d15dfb8cd
WnoonW <wonwoo0406@naver.com>
2025-03-04 16:01:27 +0900

    aa

---
+++ 3월/1주차/03_03LikeLion/03_03LikeLion/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _03_03LikeLion
+{
+    /*public class Player
+    {
+        public string Name {get;set;}
+        public int Score {get;set;}
+    }
+    public class Warrior : Player
+    {
+        public int Strength {get;set;}
+    }
+    public class Enemy : IAttackbale, IMoniker
+    {
+        public void Attack() { }
+        public void Move() { }
+    }*/
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("프로그램 종료");
+            string path = Environment.GetEnvironmentVariable("PATH");
+            Console.WriteLine($"PATH: {path}");
+            Environment.Exit(0);*/
+
+
+            /*Random rand = new Random();
+            int randomNumber = rand.Next(1, 101);
+            Console.WriteLine("랜덤 숫자 : " + randomNumber);*/
+
+
+            /*Stopwatch stopwatch = Stopwatch.StartNew();
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(1);
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine("for문 시간 : " + stopwatch.ElapsedMilliseconds + "ms");*/
+
+
+            /*string input = "123-456-7890";
+            string pattern = @"^\d{3}-\d{3}-\d{4}$";
+            bool isMatch = Regex.IsMatch(input, pattern);
+            Console.WriteLine($"Is valid phone number: {isMatch}");*/
+
+            //값 형식과 참조형식
+            //값 형식은 스택, 참조형식은 힙
+
+            /*int valueType = 10;
+            object referenceType = valueType;
+            valueType = 20;
+
+            Console.WriteLine($"ValueType : {valueType}");
+            Console.WriteLine($"ReferenceType : {referenceType}");*/
+
+            /*object obj = 42;
+            var obj2 = 42;
+
+            if (obj is int number)
+            {
+                Console.WriteLine($"Number : {number}");
+            }
+            else
+            {
+                Console.WriteLine("Not a number");
+            }*/
+
+            //string 다양한 메소드
+            /*
+                        string text = "C# is awesome!";
+                        Console.WriteLine($"Contains 'awesome' : {text.Contains("awesome")}");
+                        Console.WriteLine($"Starts with 'C#' : {text.StartsWith("C#")}");
+                        Console.WriteLine($"Index of 'is' : {text.IndexOf("is")}");
+                        Console.WriteLine($"Replace 'awesome' with 'great' : {text.Replace("awesome", "great")}");*/
+
+            /*int iterations = 10000;
+            Stopwatch sw = Stopwatch.StartNew();
+            string text = "";
+            for (int i = 0; i < iterations; i++)
+            {
+                text += "a";
+            }
+            sw.Stop();
+            Console.WriteLine($"String: {sw.ElapsedMilliseconds} ms");
+
+            sw.Restart();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < iterations; i++)
+            {
+                sb.Append("a");
+            }
+            sw.Stop();
+            Console.WriteLine($"StringBuilder: {sw.ElapsedMilliseconds} ms");*/
+
+            /* try
+             {
+                 int[] numbers = { 1, 2, 3 };
+                 Console.WriteLine(numbers[5]);   //오류발생
+             }
+             catch (Exception ex)
+             {
+                 Console.WriteLine($"Error: {ex.Message}");
+             }
+             try
+             {
+                 int number = int.Parse("NotANumber"); 
+             }
+             catch (Exception ex)
+             {
+                 Console.WriteLine($"Format Error: {ex.Message}");
+             }
+             finally
+             {
+                 Console.WriteLine("Execution finished.");
+             }*/
+
+
+            /*try
+            {
+                int age = -5;
+                if (age < 0)
+                {
+                    throw new ArgumentException("Age cannot be negative");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception :{ex.Message}");
+            }*/
+
+
+        }
+    }
+}
---
+++ 3월/1주차/03_03LikeLion/03_03LikeLion1/Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_03LikeLion1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /*int[] numbers = { 1, 2, 3, 4, 5 };
+            foreach (var num in numbers)
+            {
+                Console.Write(num+" ");
+            }
+
+            List<string> names = new List<string> { "Alice", "Bob", "Charlie" };
+            names.Add("Dave");
+            names.Remove("Bob");
+
+            foreach (var name in names)
+            {
+                Console.Write(name+ " ");
+            }*/
+
+            /*List<int> list = new List<int>();
+            
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+            list.Insert(1, 100);
+            Console.WriteLine(list[1]);
+            Console.WriteLine(list.Count);*/
+
+
+            /*Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(1);
+
+            while (stack.Count > 0)
+            {
+                Console.WriteLine(stack.Pop());
+            }*/
+
+            /*Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            while (queue.Count > 0)
+            {
+                Console.WriteLine(queue.Dequeue());
+            }*/
+
+            /*ArrayList arrayList = new ArrayList();
+
+            arrayList.Add(1);
+            arrayList.Add("hi");
+            Console.WriteLine("ArrayList 요소:");
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+            arrayList.Remove(1);
+            Console.WriteLine("ArrayList 요소제거 후:");
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }*/
+
+            /*Hashtable hashtable = new Hashtable();
+
+            hashtable["Alice"] = 25;
+            hashtable["Bob"] = 30;
+            hashtable["Charlie"] = 35;*/
+
+
+
+            /*Cup<string> cupOfString = new Cup<string> { Content = "Coffee" };
+            Cup<int> cupOfInt = new Cup<int> { Content = 42 };
+            Console.WriteLine($"CupOfString: {cupOfString.Content}");
+            Console.WriteLine($"CupOfInt: {cupOfInt.Content}");*/
+
+
+            /*List<string> list = new List<string> { "Alice", "Bob", "Charlie" };
+            list.Add("David");
+            list.Remove("Bob");
+            foreach (string n in list)
+            {
+                Console.WriteLine(n);
+            }*/
+
+
+
+            /*ArrayList list = new ArrayList { "Apple", "Banana", "Cherry" };
+            IEnumerator enumerator = list.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }*/
+
+
+            /*var collection = new SimpleCollection();
+            foreach (var i in collection)
+            {
+                Console.WriteLine(i);
+            }*/
+
+
+            /*Dictionary<string, int> ages = new Dictionary<string, int>();
+
+            ages["금도끼"] = 10;
+            ages["은도끼"] = 5;
+            ages["돌도끼"] = 1;
+
+            foreach (var pair in ages)
+            {
+                Console.WriteLine($"{pair.Key} : {pair.Value}");
+            }*/
+
+
+
+            /*string str = null;
+            if (str == null)
+            {
+                Console.WriteLine("str is null");
+            }*/
+
+            /*//값형식에서 null쓰고싶을때
+            int? nullableInt = null;
+            Console.WriteLine(nullableInt.HasValue ? nullableInt.Value.ToString() : "No value");
+            nullableInt = 10;
+            Console.WriteLine(nullableInt.HasValue ? nullableInt.Value.ToString() : "No value");*/
+
+
+            /*string str = null;
+            Console.WriteLine(str??"DefaultValue"); //str이 null이면 출력
+            str = "Hello";
+            Console.WriteLine(str?.Length); //str이 null 아니면 길이 출력*/
+
+
+            //LINQ는 확장메서드 형태로 제공된다. 
+            //LINQ(Language Integrated Query)를 사용해 컬렉션을 쿼리할 수있습니다.
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            var evenNumbers = numbers.Where(n => n % 2 == 0);
+
+            foreach (var num in evenNumbers)
+            {
+                Console.WriteLine(num);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+        /*class Cup<T>
+        {
+            public T Content { get; set; }
+        }*/
+        /*class SimpleCollection : IEnumerable<int>
+        {
+            private int[] data = { 1, 2, 3, 4, 5 };
+            public IEnumerator<int> GetEnumerator()
+            {
+                foreach (var item in data)
+                {
+                    yield return item;
+                }
+            }
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }*/
+}
+}
